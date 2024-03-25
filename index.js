2377f62d897258add69b68f68d42c4a5f02a0540
Irfan Ali <ifiia258@gmail.com>
2024-03-25 17:48:41 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "please try to guess a number between 1-10:",
+    },
+]);
+if (answers.UserGuessedNumber == randomNumber) {
+    console.log("welldone! you guessed the right number");
+}
+else {
+    console.log("unfortunately, that's not the right number you guessed");
+}
